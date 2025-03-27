e1c341b49cbb9bfe71668d1f4ff56b768ebe310a
alexcekay <a-lerach@live.de>
2025-03-27 17:21:12 +0100

    dataman: clarify default storage backend

--- src/modules/dataman/parameters.c
+++ src/modules/dataman/parameters.c
@@ -34,10 +34,13 @@
 /**
  * Dataman storage backend
  *
+ * If the board supports persistent storage (i.e., the KConfig variable DATAMAN_PERSISTENT_STORAGE is set),
+ * the 'Default storage' backend uses a file on persistent storage. If not supported, it uses non-persistent storage in RAM.
+ *
  * @group System
  * @value -1 Disabled
- * @value 0 default (SD card)
- * @value 1 RAM (not persistent)
+ * @value 0 Default storage
+ * @value 1 RAM storage
  * @boolean
  * @reboot_required true
  */
